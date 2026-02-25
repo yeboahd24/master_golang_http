ca3e3953cbda0294ca86d6a3725eb06f12c4a03a
Dominic Kofi Yeboah <yeboahd24@gmail.com>
2026-02-25 12:14:30 +0000

    select

--- Goroutine/select.go
+++ Goroutine/select.go
@@ -4,13 +4,14 @@ import "sync"
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Wait()
 	ch1 := make(chan int, 1)
 	ch2 := make(chan int, 1)
 	ch3 := make(chan int, 1)
 
+	wg.Add(1)
+
 	go func() {
+		defer wg.Done()
 		for {
 			select {
 			case ch1 <- 1:
@@ -35,4 +36,5 @@ func main() {
 			}
 		}
 	}()
+	wg.Wait()
 }
